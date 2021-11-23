24988af367c617328b9a062ed36c09f5e8f2b0d6
Jo Wagner <jowagner@limago.de>
2021-11-23 13:35:45 +0100

    Initial commit

--- TakeGame/src/test/java/de/techem/games/takegame/player/ComputerPlayerTest.java
+++ TakeGame/src/test/java/de/techem/games/takegame/player/ComputerPlayerTest.java
@@ -2,6 +2,8 @@ package de.techem.games.takegame.player;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -19,27 +21,13 @@ class ComputerPlayerTest {
 	@Mock
 	private Writer writer;
 	
-	@Test
-	void doTurn_divisionsRest0_returns3 () {
-		assertEquals(3, objectUnderTest.doTurn(20));
-		verify(writer,times(1)).write("Computer nimmt 3 Steine.");
-	}
-	@Test
-	void doTurn_divisionsRest1_returns1() {
-		assertEquals(1, objectUnderTest.doTurn(21));
-		verify(writer,times(1)).write("Computer nimmt 1 Steine.");
+	@ParameterizedTest
+	@CsvSource({"20,3","21,1","22,1","23,2"})
+	void doTurn_divisionsRest0_returns3 (int input, int expected) {
+
+		assertEquals(expected, objectUnderTest.doTurn(input));
+		verify(writer,times(1)).write("Computer nimmt "+ expected+ " Steine.");
 	}
 
-	@Test
-	void doTurn_divisionsRest2_returns1() {
-		assertEquals(1, objectUnderTest.doTurn(22));
-		verify(writer,times(1)).write("Computer nimmt 1 Steine.");
-	}
-
-	@Test
-	void doTurn_divisionsRest3_returns2() {
-		assertEquals(2, objectUnderTest.doTurn(23));
-		verify(writer,times(1)).write("Computer nimmt 2 Steine.");
-	}
 
 }
